dc7e2215fac8fd7ba4f8fa361d74c091721f4519
sl <v.cherenev@seuslab.ru>
2024-06-07 13:41:17 +0500

    Recursive cells opening.

--- miner.js
+++ miner.js
@@ -15,8 +15,9 @@ function initField() {
         fieldArray[row] = []
         for (let column = 0; column < MAX_COLUMNS; column++) {
             const mineOrEmpty = getMineOrEmpty();
-            rowContainer.append(createCell(row, column, mineOrEmpty))
-            fieldArray[row][column] = mineOrEmpty;
+            let cell = (createCell(row, column, mineOrEmpty));
+            rowContainer.append(cell);
+            fieldArray[row][column] = {mineOrEmpty: mineOrEmpty, cell: cell};
         }
         field.append(rowContainer);
     }
@@ -40,12 +41,17 @@ function createCell(row, column, mineOrEmpty) {
 
 function cellClickHandler(e) {
     e.preventDefault();
+
+    const cell = e.target;
+    const row = cell.getAttribute('data-row')
+    const column = cell.getAttribute('data-column')
+
     switch (e.buttons) {
         case 0: // left button
-            openCell(e.target);
+            openCellRecursive(row, column);
             break;
         case 2: // right button
-            markCell(e.target);
+            markCell(cell);
             break;
     }
     return false;
@@ -59,27 +65,51 @@ function markCell(e) {
     }
 }
 
-function openCell(e) {
-    e.classList.remove("closed");
-    e.classList.add("opened");
-    const row = e.getAttribute('data-row')
-    const column = e.getAttribute('data-column')
-    if (fieldArray[row][column] === "mine") {
-      alert("GameOver!");
-    } else {
-        let mineCount = calcNeighbours(row, column);
-        e.innerHTML = mineCount;
-        e.classList.add(`count-${mineCount}`)
+function openCellRecursive(row, column) {
+    openCell(row, column);
+    if (isMine(row, column)) {
+        overGame();
+    } else if (calcNeighbours(row, column) === 0) {
+        for (let dx = 0; dx < 3; dx++) {
+            for (let dy = 0; dy < 3; dy++) {
+                let r = row - 1 + dx;
+                let c = column - 1 + dy;
+                if (c >= 0 && c < MAX_COLUMNS && r >= 0 && r < MAX_ROWS) {
+                    let cell = fieldArray[r][c]['cell'];
+                    if (!isMine(r, c) && cell.classList.contains("closed")) openCellRecursive(r, c);
+                }
+            }
+        }
     }
 }
 
+function openCell(row, column) {
+    const cell = fieldArray[row][column]['cell']
+    cell.classList.remove("closed");
+    cell.classList.add("opened");
+    let mineCount = calcNeighbours(row, column);
+    if (mineCount > 0) {
+        cell.innerHTML = mineCount;
+        cell.classList.add(`count-${mineCount}`)
+    }
+}
+
+function overGame() {
+    alert("GameOver!");
+}
+
+function isMine(row, column) {
+    return fieldArray[row][column]['mineOrEmpty'] === "mine"
+}
+
 function calcNeighbours(row, column) {
+    if (isMine(row, column)) return -1;
     let mineCount = 0;
     for (let dx = 0; dx < 3; dx++) {
         for (let dy = 0; dy < 3; dy++) {
             let r = row - 1 + dx;
             let c = column - 1 + dy;
-            if (c >= 0 && c < MAX_COLUMNS && r >= 0 && r < MAX_ROWS && fieldArray[r][c] === "mine") mineCount++;
+            if (c >= 0 && c < MAX_COLUMNS && r >= 0 && r < MAX_ROWS && isMine(r, c)) mineCount++;
         }
     }
     return mineCount;
